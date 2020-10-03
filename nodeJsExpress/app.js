3c796df84cc595de3ba1cd5095f6e849c6a79125
KoheiNomura <kohei.nomura01@g.softbank.co.jp>
2020-10-03 23:43:22 +0900

    feat: new

--- nodeJsExpress/app.js
+++ nodeJsExpress/app.js
@@ -1,4 +1,8 @@
 const express = require("express");
 const app = express();
 
+app.use(require("./lib/logger.js"));
+app.get("/", (req, res) =>{
+    res.status(200).send("hello");
+});
 app.listen(3000);
