3cd5c977ead4be9ae101099819db42232a69fb5a
KoheiNomura <kohei.nomura01@g.softbank.co.jp>
2020-10-13 19:29:40 +0900

    feat: watch

--- understanding-ts/app.js
+++ understanding-ts/app.js
@@ -1,21 +1,12 @@
-function combine(input1, input2, resultConversion) {
-    var result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    }
-    else {
-        result = input1.toString() + input2.toString();
-    }
-    /* if(resultConversion === 'as-number'){
-        return +result;
-    } else {
-        return result.toString();
-    } */
-    return result;
+"use strict";
+var userInput;
+var userName;
+userInput = 5;
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
-var combinedAges = combine(30, 26, 'as-number');
-console.log(combinedAges);
-var combinedStringAges = combine('30', '26', 'as-number');
-console.log(combinedStringAges);
-var combinedNames = combine('Max', 'Anna', 'as-text');
-console.log(combinedNames);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+}
+//const result = generateError('エラー発生', 500);
+//console.log(result);
