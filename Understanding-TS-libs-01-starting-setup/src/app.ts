57062e2763a8b928499f62614f9636f4f9e03fec
KoheiNomura <kohei.nomura01@g.softbank.co.jp>
2020-10-27 11:28:08 +0900

    feat: class-validator

--- Understanding-TS-libs-01-starting-setup/src/app.ts
+++ Understanding-TS-libs-01-starting-setup/src/app.ts
@@ -1,12 +1,23 @@
 // ここにコードを書きます
 import "reflect-metadata";
 import { plainToClass } from 'class-transformer';
+import { validate } from 'class-validator';
 import { Product } from './product.model';
 
 const products = [
     { title: "商品１", price: 100 },
     { title: "商品２", price: 200 },
 ];
+
+const newProd = new Product('', -100);
+validate(newProd).then(errors => {
+    if(errors.length > 0){
+        console.log("バリデーションエラー");
+        console.log(errors);
+    } else {
+        console.log(newProd.getInformation());
+    }
+});
 
 // const p1 = new Product('商品１', 100);
 
