4ccac96f53377b066e3a0405786a524753f93f10
KoheiNomura <kohei.nomura01@g.softbank.co.jp>
2020-10-31 17:36:34 +0900

    implement event creation

--- react-hooks-101/src/reducers/index.js
+++ react-hooks-101/src/reducers/index.js
@@ -15,16 +15,16 @@ state = [
     }
 ] */
 
-state = []
+/* state = []
 state = [
     {id: 1, title: 'タイトル1', body: 'ボディー1'},
     {id: 2, title: 'タイトル2', body: 'ボディー2'},
     {id: 3, title: 'タイトル3', body: 'ボディー3'},
-]
+] */
 
 const events = (state = [], action) => {
     switch(action.type){
-        case 'CERATE_EVENT':
+        case 'CREATE_EVENT':
             const event = { title: action.title, body: action.body }
             const length = state.length
             const id = length === 0 ? 1: state[length - 1].id + 1 
