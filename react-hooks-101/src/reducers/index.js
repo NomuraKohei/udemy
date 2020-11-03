05ff24210dd317ab41530891e957d8785091c9d3
KoheiNomura <kohei.nomura01@g.softbank.co.jp>
2020-11-03 17:29:41 +0900

    create operationLogs reducer

--- react-hooks-101/src/reducers/index.js
+++ react-hooks-101/src/reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from 'redux'
 import events from './events'
+import operationLogs from './operationLogs'
 
-combineReducers({events})
-
-export default combineReducers({events})
+export default combineReducers({
+    events, operationLogs
+})
