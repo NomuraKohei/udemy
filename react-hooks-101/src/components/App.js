d461465d41945afb7f07267cab1122196de3c512
KoheiNomura <kohei.nomura01@g.softbank.co.jp>
2020-11-03 16:46:32 +0900

    setup react comtext provider

--- react-hooks-101/src/components/App.js
+++ react-hooks-101/src/components/App.js
@@ -4,16 +4,19 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import EventForm from './EventForm'
 import Events from './Events'
 import reducer from '../reducers'
+import AppContext from '../contexts/AppContext'
 
 const App = () => {
   const [state, dispatch] = useReducer(reducer, [])
   console.log(state)
 
   return (
-    <div className="container-fluid">
-      <EventForm state={state} dispatch={dispatch}/>
-      <Events state={state} dispatch={dispatch}/>
-    </div>
+    <AppContext.Provider value={'hello'}>
+      <div className="container-fluid">
+        <EventForm state={state} dispatch={dispatch}/>
+        <Events state={state} dispatch={dispatch}/>
+      </div>
+    </AppContext.Provider>
   );
 }
 
